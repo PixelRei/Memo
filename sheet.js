b72743db746c4fbbccb5c9a86e1912dc2b04d3b9
PixelRei <playboi.rei17@gmail.com>
2025-05-30 22:59:22 +0200

    Add files via upload

--- sheet.js
+++ sheet.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const start = document.getElementById("ButtonStart");
+    const first = document.querySelector(".start");
+    const second = document.querySelector(".test");
 
+    
+    start.addEventListener("click", function(){
+        first.style.display = "none";
+        second.style.display = "flex";
+    });
+});
